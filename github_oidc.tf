7030d8d4218b35fb3e98852592a8801e799c60c4
Aloisio Barbosa <rodhat7@gmail.com>
2026-08-12 15:39:09 -0300

    fix: oidc ec2:DisassociateAddress

--- github_oidc.tf
+++ github_oidc.tf
@@ -75,6 +75,7 @@ data "aws_iam_policy_document" "github_actions_infra_network" {
       "ec2:DescribeVpcs",
       "ec2:DetachInternetGateway",
       "ec2:DisassociateRouteTable",
+      "ec2:DisassociateAddress",
       "ec2:ModifySubnetAttribute",
       "ec2:ModifyVpcAttribute",
       "ec2:ReleaseAddress",
